bc440aa7b3a2291a94ea66f64987c9bf6cb2e431
Mana Sotomura <mana.tay13@gmail.com>
2022-06-14 19:33:06 +0900

    Initial Commit

---
+++ AnnotationApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnnotationApp2
+//
+//  Created by 外村真那 on 2022/06/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
